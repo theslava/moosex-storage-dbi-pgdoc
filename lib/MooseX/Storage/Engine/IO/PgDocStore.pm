783512eb31ab4ad9749cbf9312f4f54dc8216566
Slava Goltser <slavikg@gmail.com>
2015-11-30 21:05:34 -0500

    add Postgres document store role and engine

---
+++ lib/MooseX/Storage/Engine/IO/PgDocStore.pm
@@ -0,0 +1,57 @@
+package MooseX::Storage::Engine::IO::PgDocStore;
+
+our $VERSION = '0.01';
+
+use Moose;
+use namespace::autoclean;
+
+use Carp;
+use DBI;
+
+has 'dbh' => (
+    is => 'ro',
+    isa => 'DBI::db',
+    required => 1,
+);
+
+has 'uuid' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+sub load {
+    my ($self) = @_;
+
+my $load_sql = <<EOF;
+    SELECT json
+    FROM json
+    WHERE uuid=?
+    LIMIT 1
+EOF
+
+    my $sth = $self->dbh->prepare($load_sql);
+    my $rv = $sth->execute($self->uuid);
+    if (!$rv) {
+        croak "Error loading object ($self->uuid): $sth->err";
+    }
+    return $self->dbh->selectrow_array($sth);
+}
+
+sub store {
+    my ($self, $json) = @_;
+
+my $store_sql = <<EOF;
+    INSERT INTO json (uuid, json)
+        VALUES (?, ?)
+EOF
+
+    my $sth = $self->dbh->prepare($store_sql);
+    my $rv = $sth->execute($self->uuid, $json);
+    if (!$rv) {
+        croak "Error storing object ($self->uuid): $sth->err";
+    }
+    return $rv;
+}
+
+1;
---
+++ lib/MooseX/Storage/IO/PgDocStore.pm
@@ -0,0 +1,25 @@
+package MooseX::Storage::IO::PgDocStore;
+
+our $VERSION = '0.01';
+
+use Moose::Role;
+use MooseX::Storage::Engine::IO::PgDocStore;
+use namespace::autoclean;
+
+requires 'freeze';
+requires 'thaw';
+requires 'uuid';
+
+sub load {
+    my ($class, $dbh, $uuid, @args) = @_;
+    $class->thaw( MooseX::Storage::Engine::IO::PgDocStore->new( dbh => $dbh, uuid => $uuid)->load(), @args);
+}
+
+sub store {
+    my ($self, $dbh, @args) = @_;
+    MooseX::Storage::Engine::IO::PgDocStore->new( dbh => $dbh, uuid => $self->uuid )->store( $self->freeze );
+}
+
+no Moose::Role;
+
+1;
